ff50ffa4ae38193cac321f6cadedd733423e81cd
Hesam Hosseini <hesamhosseini@Hesams-MacBook-Air.local>
2024-04-25 20:12:24 +0330

    greeting string changed

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -12,10 +12,11 @@ const greeting = () => async (ctx: Context) => {
   debug('Triggered "greeting" text command');
 
   const messageId = ctx.message?.message_id;
-  const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
+
+  const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name ? ctx.message?.from.last_name  : ""}`;
 
   if (messageId) {
-    await replyToMessage(ctx, messageId, `Hello, ${userName}!`);
+    await replyToMessage(ctx, messageId, `سلام جیگرررر چی کار کنم برات ${userName}!`);
   }
 };
 
